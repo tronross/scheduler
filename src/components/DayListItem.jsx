ef217325aff4275fb5ba522be1ec986ffd0c9084
tronross <john_ross@analogforever.ca>
2023-01-25 13:39:12 -0500

    Add DayListItem component (initial)

---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,14 @@
+/////////////////////////////////
+// DayListItem Component
+/////////////////////////////////
+import React from "react";
+
+// Component
+export default function DayListItem(props) {
+  return (
+    <li>
+      <h2 className="text--regular">Day Name</h2>
+      <h3 className="text--light">X spots remaining</h3>
+    </li>
+  );
+}
