b032b057f681da73717118a6c9403ac99394c537
tronross <john_ross@analogforever.ca>
2023-01-25 18:59:17 -0500

    Add DayList component (initial)

---
+++ src/components/DayList.jsx
@@ -0,0 +1,13 @@
+/////////////////////////
+// DayList Component
+/////////////////////////
+import React from "react";
+import DayListItem from "./DayListItem";
+
+// Component
+export default function DayList(props) {
+
+  return (
+    <ul></ul>
+  );
+}
