dbecfddd9cf92889d86bcc141fb1b63b9e60e44a
tronross <john_ross@analogforever.ca>
2023-01-25 19:43:06 -0500

    Add days.map() and return <ul>{dayList},/ul> to DayList

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -6,8 +6,20 @@ import DayListItem from "./DayListItem";
 
 // Component
 export default function DayList(props) {
+  const days = props.days;
+  
+  const dayList = days.map(day => {
+    return(
+      <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />)
+  })  
 
   return (
-    <ul></ul>
+    <ul>{dayList}</ul>
   );
 }
