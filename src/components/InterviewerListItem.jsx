950dc5d1dbd3defa8add027ad5ac308e40011d79
tronross <john_ross@analogforever.ca>
2023-01-26 17:56:23 -0500

    Add classNames -> interviewerClass

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -2,15 +2,17 @@
 // InterviewerListItem Component
 /////////////////////////////////
 import React from "react";
+import classNames from "classnames";
 
 // Stylesheet
+import "components/InterviewerListItem.scss"
 
 // Component
 export default function InterviewerListItem(props)  {
-
+  const interviewerClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected});
 
   return (
-    <li className="interviewers__item">
+    <li className="interviewers__item" onClick={() => props.setInterviewer(props.id)}>
       <img
         className="interviewers__item-image"
         src="https://i.imgur.com/LpaY82x.png"
