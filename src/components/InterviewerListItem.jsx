f2f925b836726066d273038b82548a9b57644d0b
tronross <john_ross@analogforever.ca>
2023-01-27 15:56:58 -0500

    Fix img className

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -23,7 +23,7 @@ export default function InterviewerListItem(props)  {
   return (
     <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
       <img
-        className={interviewerClass}
+        className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
