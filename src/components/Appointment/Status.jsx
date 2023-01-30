e8d278f98b468e277c5fb15ac25b9af70c4e9553
tronross <john_ross@analogforever.ca>
2023-01-29 20:33:47 -0500

    Add comments; improve legibility

--- src/components/Appointment/Status.jsx
+++ src/components/Appointment/Status.jsx
@@ -3,6 +3,7 @@
 ////////////////////////////////////
 import React from "react";
 
+// Component
 export default function Status(props) {
   return (
     <main className="appointment__card appointment__card--status">
