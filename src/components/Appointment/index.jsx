b254e49342f595ec834560e6de92d76a9c50d15a
tronross <john_ross@analogforever.ca>
2023-01-29 20:14:37 -0500

    Add props and child components to Appointment; destructure incoming props

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,17 +1,36 @@
 ////////////////////////////
 // Appointment Component
 ////////////////////////////
-import React, { useState } from "react";
+import React, { Fragment } from "react";
 
 // Stylesheet
 import "./styles.scss"
 
 // Child Components
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-
+  const interview = {...props.interview};
+  const interviewerObj = {...interview.interviewer};
 
   return (
-    <article className="appointment"></article>
+    <Fragment >
+      <Header time={props.time} />
+      <article className="appointment" >
+        <>
+          {props.interview ?
+            <>
+              <Show student={interview.student} interviewer={interviewerObj.name} />
+            </>
+            :
+            <>
+              <Empty />
+            </>
+          }
+        </>
+      </article>
+    </Fragment>
   );
-} 
+}
