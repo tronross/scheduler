75e0d675d0ab7348b3784d5a478b1570a64c1adc
tronross <john_ross@analogforever.ca>
2023-01-31 17:03:17 -0500

    Add .map(appointments) -> <Appointment /> to populate schedule in app

--- src/components/Application.js
+++ src/components/Application.js
@@ -79,26 +79,34 @@ export default function Application(props) {
     <main className="layout">
       <section className="sidebar">
         <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList
-  days={days}
-  value={day}
-  onChange={setDay}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+          <nav className="sidebar__menu">
+            <DayList
+              days={days}
+              value={day}
+              onChange={setDay}
+            />
+          </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment) => {
+          return (
+          <Appointment
+          key={appointment.id}
+          {...appointment}
+          />
+          )}
+        )}
+          <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
