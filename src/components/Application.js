a4df167c31d46da8fa372e44a437ea7c7ff1595f
tronross <john_ross@analogforever.ca>
2023-02-02 17:41:34 -0500

    Add setDay, setDays functions; refactor component props; refactor axios GET /api/days

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,14 +56,19 @@ const appointments = {
 
 // Component
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // State
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  })
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({...prev, days}));
 
   useEffect(() => {
     axios
     .get('/api/days')
     .then((response) => {
-      setDays([...response.data])
+      setDays(response.data)
     })
   }, [])
   
@@ -78,8 +83,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
             <DayList
-              days={days}
-              value={day}
+              days={state.days}
+              value={state.day}
               onChange={setDay}
             />
           </nav>
