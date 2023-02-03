4b82781e19f524461f79acadf77c6ff3eeec6b8b
tronross <john_ross@analogforever.ca>
2023-02-02 23:01:46 -0500

    Fix setState appointments; refactor getAppointmentsForDay

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,49 +10,7 @@ import Appointment from "./Appointment";
 
 // Stylesheet
 import "components/Application.scss";
-
-// Mock Data
-
-// Appointments
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
-
+import { getAppointmentsForDay } from "helpers/selectors";
 
 // Component
 export default function Application(props) {
@@ -63,10 +21,10 @@ export default function Application(props) {
     appointments: {}
   })
 
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({...prev, days}));
+  
 
   // Routes
   useEffect(() => {
@@ -74,8 +32,7 @@ export default function Application(props) {
       axios.get('/api/days'),
       axios.get('/api/appointments')
     ]).then((responses) => {
-      console.log(responses[1].data)
-      setState(prev => ({...prev, days: responses[0].data, dailyAppointments: responses[1].data}));
+      setState(prev => ({...prev, days: responses[0].data, appointments: responses[1].data}));
     });
   }, [])
   
