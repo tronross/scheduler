5c0a2f7dee9147751c5e674490dcac1336d6318c
tronross <john_ross@analogforever.ca>
2023-02-03 16:42:51 -0500

    Add/fix logic to getInterview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -18,12 +18,37 @@ export function getAppointmentsForDay(state, day) {
 
   const appointmentsForDay = appointmentsIds.map((appId) => {
     return state.appointments[appId]
-  })
+  });
 
   return appointmentsForDay
 };
 
 // getInterview
 export function getInterview(state, interview) {
+  const interviewers = state.interviewers;
+  let interviewerKey;
+
+  // edge case error handling
+  if (!interview) {
+    return null;
+  }
   
-}
+  for (const intKey in interviewers) {
+    if (Number(intKey) === interview.interviewer) {
+      interviewerKey = intKey;
+    }
+  }
+  
+  const interviewer = interviewers[interviewerKey];
+  
+  const interviewObj = {
+    student:      interview.student,
+    interviewer: {
+                  id: interviewer.id,
+                  name: interviewer.name,
+                  avatar: interviewer.avatar
+                 }
+  };
+
+  return interviewObj;
+};
