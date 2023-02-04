9a0edfe18419584ab05715b94eabdabbdc818d53
tronross <john_ross@analogforever.ca>
2023-02-03 22:41:31 -0500

    Add back function (and method) and conditional logic to set a back limit

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -3,17 +3,27 @@
 ////////////////////////////
 import React, { useState, useEffect } from "react";
 
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
 
+  const transition = function(newMode, replace) {
+    
+    history.push(newMode);
+    
+    setMode(history[history.length - 1]);
+  }
 
-export default function useVisualMode(visualMode) {
-  const [mode, setMode] = useState(visualMode);
-
-  const transition = function(newMode) {
-    setMode(newMode);
+  const back = function() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
   }
 
   return { 
             mode,
-            transition
+            transition,
+            back
          };
 }
