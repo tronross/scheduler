870ad1637c84c8af8403313c3be00e058eebc82f
tronross <john_ross@analogforever.ca>
2023-02-03 22:14:16 -0500

    Add transition function to update mode (and pass as method in returned object)

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -8,5 +8,12 @@ import React, { useState, useEffect } from "react";
 export default function useVisualMode(visualMode) {
   const [mode, setMode] = useState(visualMode);
 
-  return { mode };
+  const transition = function(newMode) {
+    setMode(newMode);
+  }
+
+  return { 
+            mode,
+            transition
+         };
 }
