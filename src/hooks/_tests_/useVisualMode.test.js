e5475fb9ba562e42b2fd647e46298e2a40424358
tronross <john_ross@analogforever.ca>
2023-02-03 22:15:08 -0500

    Add transition unit test

--- src/hooks/_tests_/useVisualMode.test.js
+++ src/hooks/_tests_/useVisualMode.test.js
@@ -5,11 +5,20 @@ import { renderHook, act } from "@testing-library/react-hooks";
 
 import useVisualMode from "hooks/useVisualMode";
 
+// Mode constants
 const FIRST = "FIRST";
+const SECOND = "SECOND";
 
 // Unit tests
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
 });
