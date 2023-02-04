d59d8baea994a747d23570bb20b505a50d639f26
tronross <john_ross@analogforever.ca>
2023-02-03 21:49:41 -0500

    Add logic for setting initial mode state

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -1,3 +1,12 @@
 ////////////////////////////
 // useVisualMode Hook
 ////////////////////////////
+import React, { useState, useEffect } from "react";
+
+
+
+export default function useVisualMode(visualMode) {
+  const [mode, setMode] = useState(visualMode);
+
+  return { mode };
+}
