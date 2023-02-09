e5be5a176c2de851ada6d645dbad3a27795d0a3a
tronross <john_ross@analogforever.ca>
2023-02-08 19:08:04 -0500

    Remove redundant import

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -2,7 +2,7 @@
 // useVisualMode Hook
 ////////////////////////////
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
