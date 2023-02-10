fd6ef7eb253c5de5940a40df7cdb34be0fdb622d
tronross <john_ross@analogforever.ca>
2023-02-09 22:41:53 -0500

    Refactor transition (fix stale state); refactor returned mode

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -10,24 +10,28 @@ export default function useVisualMode(initialMode) {
 
   const transition = function(newMode, replace = false) {
     if (replace) {
-      history.pop();
-      setMode(newMode);
-      setHistory(prev => [...prev, newMode]);
-    };
     
-    setMode(newMode);
-    history.push(newMode);
+
+     
+      setHistory(prev => [...prev.slice(0, -1), newMode]);
+      
+    } else {
+     
+      setHistory(prev => [...prev, newMode]);
+    }
   }
 
   const back = function() {
     if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1]);
+      console.log(history);
+      setHistory(prev => prev.slice(0, -1));
+      console.log(history);
+      
     }
   }
 
   return { 
-            mode,
+            mode: history[history.length -1 ],
             transition,
             back
          };
