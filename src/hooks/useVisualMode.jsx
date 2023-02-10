a8d4cff573d1d4b0bd6b0c12de58e199b9688e79
tronross <john_ross@analogforever.ca>
2023-02-09 22:43:30 -0500

    Remove test code; lint

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -10,23 +10,15 @@ export default function useVisualMode(initialMode) {
 
   const transition = function(newMode, replace = false) {
     if (replace) {
-    
-
-     
       setHistory(prev => [...prev.slice(0, -1), newMode]);
-      
     } else {
-     
       setHistory(prev => [...prev, newMode]);
     }
   }
 
   const back = function() {
     if (history.length > 1) {
-      console.log(history);
       setHistory(prev => prev.slice(0, -1));
-      console.log(history);
-      
     }
   }
 
