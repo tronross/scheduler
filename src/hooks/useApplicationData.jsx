2081dca67df9572ac76121b03bfcc750a15c87eb
tronross <john_ross@analogforever.ca>
2023-02-10 14:15:06 -0500

    Refactor and re-order useApplicationData hook

--- src/hooks/useApplicationData.jsx
+++ src/hooks/useApplicationData.jsx
@@ -16,31 +16,19 @@ export default function useAppicationData() {
   })
 
 
-  // GET Routes, set/update state
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then((responses) => {
-      setState(prev => ({...prev, days: responses[0].data,
-                                  appointments: responses[1].data, 
-                                  interviewers: responses[2].data}));
-    });
-  }, [])
-
+  
   function bookInterview(id, interview) {
     // (Immutable) update appointments object
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
     };
-
+    
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
-
+    
     return axios.put(`/api/appointments/${id}`, {interview})
       .then(() => {
         setState({
@@ -48,40 +36,50 @@ export default function useAppicationData() {
           appointments
         })
       });
-  }
-
-  function cancelInterview(id) {
-     // (Immutable) update appointments object
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-
-    const appointments ={
-      ...state.appointments,
-      [id]:appointment
-    };
-
-    return axios.delete(`/api/appointments/${id}`)
+    }
+    
+    function cancelInterview(id) {
+      // (Immutable) update appointments object
+      const appointment = {
+        ...state.appointments[id],
+        interview: null
+      };
+      
+      const appointments ={
+        ...state.appointments,
+        [id]:appointment
+      };
+      
+      return axios.delete(`/api/appointments/${id}`)
       .then(() => {
         setState({
           ...state,
           appointments
         })
       });
-  }
-
-  // Data management
-  const setDay = day => setState({ ...state, day });
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
-  
-  return {
-            state,
-            setDay,
-            bookInterview,
+    }
+    
+    // Data management
+    const setDay = day => setState({ ...state, day });
+    // const dailyAppointments = getAppointmentsForDay(state, state.day);
+    
+    // GET Routes, set/update state
+    useEffect(() => {
+      Promise.all([
+        axios.get('/api/days'),
+        axios.get('/api/appointments'),
+        axios.get('/api/interviewers')
+      ]).then((responses) => {
+        setState(prev => ({...prev, days: responses[0].data,
+                                    appointments: responses[1].data, 
+                                    interviewers: responses[2].data}));
+      });
+    }, [])
+    
+    return {
+      state,
+      setDay,
+      bookInterview,
             cancelInterview
           };
-
-
-
 }
