f8f2ccbcbe9fa2ed495672e7eccb0fd8e5ee4894
tronross <john_ross@analogforever.ca>
2023-02-10 14:50:08 -0500

    Remove redundant code

--- src/components/Application.js
+++ src/components/Application.js
@@ -24,58 +24,9 @@ export default function Application(props) {
           bookInterview,
           cancelInterview
       } = useApplicationData();
-    // // State
-    // const [state, setState] = useState({
-      //   day: 'Monday',
-      //   days: [],
-      //   appointments: {},
-      //   interviewers: {}
-      // })
-      
-      // function bookInterview(id, interview) {
-        //   // (Immutable) update appointments object
-        //   const appointment = {
-          //     ...state.appointments[id],
-          //     interview: { ...interview }
-          //   };
-          
-  //   const appointments = {
-    //     ...state.appointments,
-    //     [id]: appointment
-    //   };
-    
-    //   return axios.put(`/api/appointments/${id}`, {interview})
-    //     .then(() => {
-      //       setState({
-        //         ...state,
-        //         appointments
-        //       })
-        //     });
-        // }
-        
-        // function cancelInterview(id) {
-          //    // (Immutable) update appointments object
-          //   const appointment = {
-            //     ...state.appointments[id],
-            //     interview: null
-            //   };
-            
-            //   const appointments ={
-              //     ...state.appointments,
-              //     [id]:appointment
-              //   };
-              
-              //   return axios.delete(`/api/appointments/${id}`)
-              //     .then(() => {
-                //       setState({
-                  //         ...state,
-                  //         appointments
-                  //       })
-                  //     });
-                  // }
-                  
+   
   // Data management
-  // const setDay = day => setState({ ...state, day });
+ 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const interviewers = getInterviewersForDay(state, state.day);
