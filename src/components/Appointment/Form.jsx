44c7ce50faa76d6039d2f4a04b6db36a69e804d8
tronross <john_ross@analogforever.ca>
2023-02-16 20:53:02 -0500

    Fix attribute data-testid

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -35,6 +35,7 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(e) => setStudent(e.target.value)}
+            data-testid="student-name-input"
           />
         </form>
         <InterviewerList 
