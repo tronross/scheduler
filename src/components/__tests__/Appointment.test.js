d654d93f99e9b2b80c7dbe1bcd8557367668ff5a
tronross <john_ross@analogforever.ca>
2023-02-16 19:43:43 -0500

    Add Appointment render unit test

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,12 @@
+////////////////////////////
+// Appointment Unit Test
+////////////////////////////
+
+import React from 'react';
+import { render } from '@testing-library/react'
+
+import Application from 'components/Application';
+
+it('renders without crashing', () => {
+  render(<Application />);
+});
