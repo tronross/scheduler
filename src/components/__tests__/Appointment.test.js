dbee360e6b75e5263fe312151c02e27186c918eb
tronross <john_ross@analogforever.ca>
2023-02-16 19:58:53 -0500

    Add describe('Appointment') and nest render test

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -7,6 +7,10 @@ import { render } from '@testing-library/react'
 
 import Application from 'components/Application';
 
-it('renders without crashing', () => {
-  render(<Application />);
+describe('Appointment', () => {
+  it('renders without crashing', () => {
+    render(<Application />);
+  });
+
+  
 });
