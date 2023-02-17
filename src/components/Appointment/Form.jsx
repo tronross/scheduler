f67deb8ccb1c204b0adfc9c7af42f6384ad75e26
tronross <john_ross@analogforever.ca>
2023-02-17 17:10:27 -0500

    Refactor confirm Button; change capitalization in error messages

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -63,7 +63,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={(props.edit) ? props.onCancel : cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
