417ce4e24d8eab0c3c1317f70e43b8d21c7ae006
tronross <john_ross@analogforever.ca>
2023-02-17 14:40:35 -0500

    Add mock onSave functionn

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -20,6 +20,7 @@ describe('Form', () => {
   ];
 
   // Unit tests
+  //1
   it('renders without student name if not provided', () => {
     
   const { getByPlaceholderText } = render(
@@ -29,6 +30,7 @@ describe('Form', () => {
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
   });
 
+  //2
   it('renders with initial student name', () => {
 
     const { getByTestId } = render(
@@ -38,40 +40,43 @@ describe('Form', () => {
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 
-    // it("validates that the student name is not blank", () => {
-    //   /* 1. Create the mock onSave function */
-    
-    //   /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the student prop should be blank or undefined */
-    
-    //   /* 3. Click the save button */
-    
-    //   expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
-    //   expect(onSave).not.toHaveBeenCalled();
-    // });
-    
-    // it("validates that the interviewer cannot be null", () => {
-    //   /* 1. Create the mock onSave function */
-    
-    //   /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the interviewer prop should be null */
-    
-    //   /* 3. Click the save button */
-    
-    //   expect(getByText(/please select an interviewer/i)).toBeInTheDocument();
-    //   expect(onSave).not.toHaveBeenCalled();
-    // });
-    
-    it("calls onSave function when the name and interviewer is defined", () => {
-      /* 1. Create the mock onSave function */
-    
-      /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
-    
-      /* 3. Click the save button */
-    
-      expect(queryByText(/student name cannot be blank/i)).toBeNull();
-      expect(queryByText(/please select an interviewer/i)).toBeNull();
-      expect(onSave).toHaveBeenCalledTimes(1);
-      expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
-    });
+  //3
+  it("validates that the student name is not blank", () => {
+    /* 1. Create the mock onSave function */
+  
+    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the student prop should be blank or undefined */
+  
+    /* 3. Click the save button */
+  
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+  });
+  
+  //4
+  it("validates that the interviewer cannot be null", () => {
+    /* 1. Create the mock onSave function */
+  
+    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the interviewer prop should be null */
+  
+    /* 3. Click the save button */
+  
+    expect(getByText(/please select an interviewer/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+  });
+  
+  //5
+  it("calls onSave function when the name and interviewer is defined", () => {
+    /* 1. Create the mock onSave function */
+  
+    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
+  
+    /* 3. Click the save button */
+  
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(queryByText(/please select an interviewer/i)).toBeNull();
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+  });
   
   
 });
