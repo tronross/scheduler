a27b928eaa808c10cde847032a4e9178bebfc22f
tronross <john_ross@analogforever.ca>
2023-02-17 22:42:08 -0500

    Fix Appointment test

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -5,12 +5,11 @@
 import React from 'react';
 import { render } from '@testing-library/react'
 
-import Application from 'components/Application';
+import Appointment from 'components/Appointment';
 
 describe('Appointment', () => {
   it('renders without crashing', () => {
-    render(<Application />);
+    render(<Appointment />);
   });
 
-  
 });
