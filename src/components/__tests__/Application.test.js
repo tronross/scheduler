1104149647da787a718b74fe723273827d7da927
tronross <john_ross@analogforever.ca>
2023-02-17 22:44:25 -0500

    Add initial Application render test, getByText('Monday')

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -2,13 +2,15 @@
 // Application Tests
 ////////////////////////////
 
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import React from 'react';
+import { render, cleanup, waitForElement } from '@testing-library/react';
 
-import Application from "components/Application";
+import Application from 'components/Application';
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it('defaults to Monday and changes the schedule when a new day is selected', () => {
+  const { getByText} = render(<Application />);
+
+  return waitForElement(() => getByText('Monday'))
 });
