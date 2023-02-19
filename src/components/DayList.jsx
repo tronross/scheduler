a773560db85641541f6936fb672aead0a234a3ee
tronross <john_ross@analogforever.ca>
2023-02-19 17:29:45 -0500

    Add data-testid prop to DayListItem

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -18,6 +18,7 @@ export default function DayList(props) {
         spots={day.spots}
         selected={day.name === props.value}
         setDay={() => props.onChange(day.name)}
+        data-testid="day"
       />
     )
   })  
