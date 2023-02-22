057971e1bcacb67a12cd64c182c90ebadb0a6cb9
tronross <john_ross@analogforever.ca>
2023-02-22 13:23:44 -0500

    Add cypress test 'should navigate to Tuesday'

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,19 @@
-describe("Navigation", () => {
-  it("should visit root", () => {
-    cy.visit("/");
+//////////////////////////////
+// Cypress Navigation Tests
+//////////////////////////////
+
+describe('Navigation', () => {
+
+  it('should visit root', () => {
+    cy.visit('/');
   });
+
+  it('should navigate to Tuesday', () => {
+    cy.visit('/')
+ 
+    cy.contains('li', 'Tuesday')
+      .click()
+      .should('have.css', 'background-color', 'rgb(242, 242, 242)')
+  });
+
 });
