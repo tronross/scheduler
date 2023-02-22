906ca728a60ea3a84939868f157d9a86aae6bb84
tronross <john_ross@analogforever.ca>
2023-02-22 16:03:05 -0500

    Add to cypress Appointment test 'should book an interview'

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -13,8 +13,10 @@ describe('Appointments', () => {
       .first()
       .click();
 
-    cy.get('[data-testid=student-name-input]')
-      .type('Lydia Miller-Jones')
+    cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
+
+    cy.get('[alt="Sylvia Palmer"]').click();
+
   })
 
 });
