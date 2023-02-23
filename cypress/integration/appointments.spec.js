cd24c7cc1f4845a93744b491b50815bfa4a75417
tronross <john_ross@analogforever.ca>
2023-02-22 19:11:50 -0500

    Add cypress Appointment test 'should delete an interview'

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -43,7 +43,14 @@ describe('Appointments', () => {
     cy.contains('.appointment__card--show','Tori Malcolm');
   });
 
-  it('')
+  it('should cancel an interview', () => {
+
+    cy.get('[alt=Delete]')
+      .first()
+      .click({ force: true });
+
+    cy.contains('Confirm').click();    
+  });
   
 
 });
